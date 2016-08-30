bc3bf1a284e0680eed5a15d65c81adf5b807653e
Asofi <asofi94@gmail.com>
2016-08-30 16:21:17 +0300

    Плавное перемещение в мп.

---
+++ On The Walls Game/Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    const string VERSION = "v0.0.1";
+    public string roomName = "OTW";
+    public string playerPrefabName = "Player";
+    public Transform spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+
+        PhotonNetwork.ConnectUsingSettings(VERSION);
+
+	}
+
+
+    void OnJoinedLobby()
+    {
+        RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 4 };
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+        Debug.Log("Joined Lobby");
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate(playerPrefabName,
+                                  spawnPoint.position,
+                                  spawnPoint.rotation,
+                                  0);
+
+        Debug.Log("Joined Room");
+
+    }
+
+}
